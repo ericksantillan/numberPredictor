31dc072d46938c26a6a66a085721255993a0da2f
Erick Santillán <erick@Ericks-MacBook-Air.local>
2017-11-06 08:30:42 +0100

    Add of convert array to image

---
+++ UI/Tuto/Hello.py
@@ -0,0 +1,8 @@
+from Tkinter import *
+
+root = Tk()
+
+w = Label(root, text = "Hello tkinter")
+w.pack()
+
+root.mainloop()
---
+++ UI/frame.py
@@ -0,0 +1,29 @@
+from Tkinter import *
+
+class App(Frame):
+    def say_hi(self):
+        print "Helloookskskd"
+
+    def createWidgets(self):
+        self.QUIT = Button(self)
+        self.QUIT["text"] = "QUIT"
+        self.QUIT["fg"] = "red"
+        self.QUIT["command"] = self.quit
+
+        self.QUIT.pack( { "side" : "left" } )
+
+        self.hi_there = Button(self)
+        self.hi_there["text"] = "Hello"
+        self.hi_there["command"] = self.say_hi
+
+        self.hi_there.pack( { "side" : "left" } )
+
+    def __init__(self, master = None):
+        Frame.__init__(self, master)
+        self.pack()
+        self.createWidgets()
+
+root = Tk()
+app = App(master = root)
+app.mainloop()
+root.destroy()
